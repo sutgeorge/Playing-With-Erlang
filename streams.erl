6e722337c862476680b82b73ce4c258c77faf9dc
Calandrinon <georgesut@yahoo.com>
2021-06-02 09:22:42 +0300

    Use a foreach to display numbers

--- streams.erl
+++ streams.erl
@@ -1,11 +1,16 @@
 -module(streams).
--import(lists, [map/2, foldl/3]).
+-import(lists, [map/2, foldl/3, foreach/2]).
 -export([main/1]).
 
 printList(List) when List =:= [] -> io:fwrite(".~n");
 printList([Head | Tail]) ->
 	io:fwrite("~w ", [Head]),
 	printList(Tail).
+
+
+epicForeach(List) ->
+	foreach(fun(X) -> io:fwrite("EPIC! ~w ", [X]) end, List),
+	io:fwrite("~n").
 	
 
 main(Numbers) ->
@@ -13,5 +18,5 @@ main(Numbers) ->
 	io:fwrite("The transformed list: ~n"),
 	printList(Transformed),
 	SumOfElements = foldl(fun(Element, Accumulator) -> Accumulator + Element end, 0, Transformed),
-	io:fwrite("The sum of the elements: ~w~n", [SumOfElements]).
-
+	io:fwrite("The sum of the elements: ~w~n", [SumOfElements]),
+	epicForeach(Transformed).
